46a30ae62aad7bf2888ddd730ac752edffda0e7e
RubenIrimiaPerez <rubenbaloncesto10@gmail.com>
2023-11-14 13:27:34 +0100

    .

---
+++ ArchivosXML/src/lectura/LeerXML.java
@@ -0,0 +1,63 @@
+package lectura;
+
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+
+public class LeerXML {
+
+	public static void main(String[] args) {
+	DocumentBuilderFactory dbf = DocumentBuilderFactory.newDefaultInstance();
+	DocumentBuilder db;
+	Document document;
+	File file;
+	XPath xPath; 
+	NodeList nodeList;
+	String path;
+	
+	try {
+		db = dbf.newDocumentBuilder();
+		file = new File("personas.xml");
+		document = db.parse(file);
+		
+		document.normalizeDocument();
+		
+		xPath = XPathFactory.newInstance().newXPath();
+		
+		//la consulta
+		path = "//apellido";
+		
+		//los nodos resultantes de la consulta
+		nodeList = (NodeList) xPath.compile(path).evaluate(document,XPathConstants.NODESET);		
+		
+		for(int i = 0; i<nodeList.getLength();i++) {
+			Element elemento = (Element) nodeList.item(i);
+			
+			System.out.println(elemento.getElementsByTagName("nombre").item(0).getTextContent());
+			System.out.println(elemento.getElementsByTagName("apellido").item(0).getTextContent());
+			System.out.println(elemento.getElementsByTagName("apellido").item(1).getTextContent());
+			System.out.println("\n");
+	
+		}
+		
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	
+		
+
+	}
+
+}
--- Mayusculas-a-Minisculas/src/main/MayusMinus.java
+++ Mayusculas-a-Minisculas/src/main/MayusMinus.java
@@ -10,60 +10,53 @@ public class MayusMinus {
 
 	public static void main(String[] args) {
 
-		  String archivoEntrada = pedirRutaArchivo("Introduce la ruta del archivo de entrada: ");
+	
+		        String archivoEntrada = "Mayus.txt"; // Reemplaza con la ruta del archivo de entrada
 
-	        try {
-	            // Abre el archivo de entrada para lectura
-	            FileReader fileReader = new FileReader(archivoEntrada);
-	            BufferedReader bufferedReader = new BufferedReader(fileReader);
+		        try {
+		            // Abre el archivo de entrada para lectura
+		            FileReader fileReader = new FileReader(archivoEntrada);
+		            BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-	            // Genera el nombre del archivo de salida con "_inver" al final
-	            String archivoSalida = archivoEntrada.replaceFirst("[.][^.]+$", "_inver$0");
+		            // Genera el nombre del archivo de salida con "_inver" al final
+		            String archivoSalida = archivoEntrada.replaceFirst("[.][^.]+$", "_inver$0");
 
-	            // Abre el archivo de salida para escritura
-	            PrintWriter printWriter = new PrintWriter(new FileWriter(archivoSalida));
+		            // Abre el archivo de salida para escritura
+		            PrintWriter printWriter = new PrintWriter(new FileWriter(archivoSalida));
 
-	            String linea;
+		            String linea;
 
-	            // Lee el archivo de entrada línea por línea
-	            while ((linea = bufferedReader.readLine()) != null) {
-	                // Procesa cada línea y cambia mayúsculas por minúsculas y viceversa
-	                String lineaInvertida = invertirMayusculasMinusculas(linea);
+		            // Lee el archivo de entrada línea por línea
+		            while ((linea = bufferedReader.readLine()) != null) {
+		                // Procesa cada línea y cambia mayúsculas por minúsculas y viceversa
+		                String lineaInvertida = invertirMayusculasMinusculas(linea);
 
-	                // Escribe la línea procesada en el archivo de salida
-	                printWriter.println(lineaInvertida);
-	            }
+		                // Escribe la línea procesada en el archivo de salida
+		                printWriter.println(lineaInvertida);
+		            }
 
-	            // Cierra los archivos
-	            fileReader.close();
-	            printWriter.close();
+		            // Cierra los archivos
+		            fileReader.close();
+		            printWriter.close();
 
-	            System.out.println("Archivo procesado con éxito. El resultado se encuentra en " + archivoSalida);
+		            System.out.println("Archivo procesado con éxito. El resultado se encuentra en " + archivoSalida);
 
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+		    }
 
-	    // Función para cambiar mayúsculas por minúsculas y viceversa
-	    private static String invertirMayusculasMinusculas(String texto) {
-	        char[] caracteres = texto.toCharArray();
-	        for (int i = 0; i < caracteres.length; i++) {
-	            char c = caracteres[i];
-	            if (Character.isUpperCase(c)) {
-	                caracteres[i] = Character.toLowerCase(c);
-	            } else if (Character.isLowerCase(c)) {
-	                caracteres[i] = Character.toUpperCase(c);
-	            }
-	        }
-	        return new String(caracteres);
-	    }
-
-	    // Función para pedir la ruta del archivo al usuario
-	    private static String pedirRutaArchivo(String mensaje) {
-	        java.util.Scanner scanner = new java.util.Scanner(System.in);
-	        System.out.print(mensaje);
-	        return scanner.nextLine();
-	}
-
-}
+		    // Función para cambiar mayúsculas por minúsculas y viceversa
+		    private static String invertirMayusculasMinusculas(String texto) {
+		        char[] caracteres = texto.toCharArray();
+		        for (int i = 0; i < caracteres.length; i++) {
+		            char c = caracteres[i];
+		            if (Character.isUpperCase(c)) {
+		                caracteres[i] = Character.toLowerCase(c);
+		            } else if (Character.isLowerCase(c)) {
+		                caracteres[i] = Character.toUpperCase(c);
+		            }
+		        }
+		        return new String(caracteres);
+		    }
+		}
