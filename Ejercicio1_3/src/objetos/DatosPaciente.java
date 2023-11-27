73eff09602b3e69eb07a768eed500890993a4b62
RubenIrimiaPerez <rubenbaloncesto10@gmail.com>
2023-11-27 08:41:47 +0100

    1.3

---
+++ Ejercicio1_3/src/objetos/DatosPaciente.java
@@ -0,0 +1,22 @@
+package objetos;
+
+public class DatosPaciente {
+	 private Paciente paciente;
+
+	public DatosPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	
+	
+	 
+
+}
